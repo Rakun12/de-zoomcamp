0b1568aa1a62943d1ecbfd46a3ac1f9e87797c6e
RacoonRen12 <94594095+Rakun12@users.noreply.github.com>
2026-01-31 16:12:29 +0000

    adding command syntax for running ingest_data.py

--- pipeline/ingest_data.py
+++ pipeline/ingest_data.py
@@ -2,6 +2,7 @@
 # coding: utf-8
 
 import pandas as pd
+import click
 from sqlalchemy import create_engine
 from tqdm.auto import tqdm
 
@@ -32,8 +33,18 @@ parse_dates = [
     'tpep_dropoff_datetime'
 ]
 
-
-def run():
+@click.command()
+@click.option('--pg-user', default='root', help='PostgreSQL username')
+@click.option('--pg-pass', default='root', help='PostgreSQL password')
+@click.option('--pg-host', default='localhost', help='PostgreSQL host')
+@click.option('--pg-port', default=5432, help='PostgreSQL port')
+@click.option('--pg-db', default='ny_taxi', help='PostgreSQL database name')
+@click.option('--year', default=2021, type=int, help='Year of the data')
+@click.option('--month', default=1, type=int, help='Month of the data')
+@click.option('--target-table', default='yellow_taxi_data', help='Target table name')
+@click.option('--chunksize', default=100000, type=int, help='Chunk size for ingestion')
+def run(pg_user, pg_pass, pg_host, pg_port, pg_db, year, month, chunksize, target_table):
+    """Ingest NYC taxi data into PostgreSQL database"""
     pg_user = 'root'
     pg_pass = 'root'
     pg_host = 'localhost'
