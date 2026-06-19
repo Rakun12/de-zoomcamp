945a356a009941b2ba5f70516b6dd60f04ed7f9a
RacoonRen12 <94594095+Rakun12@users.noreply.github.com>
2026-06-19 07:51:06 +0000

    pipeline

--- 1. Docker and Terraform/pipeline/pipeline.py
+++ 1. Docker and Terraform/pipeline/pipeline.py
@@ -1,13 +1,14 @@
 import sys
-import pandas as pd
+import pandas
 
 print('arguments', sys.argv)
 
-month = sys.argv[1]
+month = int(sys.argv[1])
 
-df = pd.DataFrame({"day":[1,2], "num_passengers":[3,4]})
+df = pandas.DataFrame({"day": [1, 2], "num_passengers": [3, 4]})
+df['month'] = month
 print(df.head())
 
 df.to_parquet(f"output_{month}.parquet")
 
-print(f'hello pipeline!, month={month}')
+print(f'hello pipeline, month={month}')
